0b16dc74c611861f0b587603ce58ccbb6b03969d
Dr Jauregui <albertodomjauregui@hotmail.com>
2021-04-27 14:19:45 +0200

    add product

--- keystone.ts
+++ keystone.ts
@@ -6,6 +6,7 @@ import {
   withItemData,
   statelessSessions,
 } from "@keystone-next/keystone/session";
+import { Product } from "./schemas/Product";
 
 const databaseURL =
   process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits";
@@ -38,6 +39,7 @@ export default withAuth(
     },
     lists: createSchema({
       User,
+      Product,
     }),
     ui: {
       isAccessAllowed: ({ session }) => {
