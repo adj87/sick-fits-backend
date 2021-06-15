94650b27e25ab79fd2f044bd257b38a3e7b51fb4
alberto <alberto.jauregui@fujitsu.com>
2021-06-15 11:28:57 +0200

    add custom table structure

--- schemas/CartItem.ts
+++ schemas/CartItem.ts
@@ -2,6 +2,11 @@ import { text, select, integer, relationship } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 
 export const CartItem = list({
+  ui: {
+    listView: {
+      initialColumns: ['product', 'quantity', 'user'],
+    },
+  },
   fields: {
     quantity: integer({ isRequired: true, defaultValue: 1 }),
     product: relationship({ ref: 'Product' }),
